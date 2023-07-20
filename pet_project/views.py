a81d2b28d6dbd1ab3883cbe677e987e0f1f2503f
Inctinct <dmitriy.makeychik@mail.ru>
2023-07-20 10:15:34 +0300

    add put and delete method for pet-project view

--- pet_project/views.py
+++ pet_project/views.py
@@ -14,9 +14,14 @@ from drf_yasg.utils import swagger_auto_schema
 class PetProjectView(APIView):
     permission_classes = (IsAuthenticated,)
 
+    @swagger_auto_schema(
+        request_body=PetProjectGetSerializer,
+        request_method="GET",
+        responses={200: PetProjectGetSerializer},
+    )
     def get(self, request):
         project = Project.objects.prefetch_related("projecttag_set").get(
-            id=request.GET.get("project_id")
+            id=request.GET.get("project_id"), user_id=request.user
         )
         data = model_to_dict(project)
         data["tags"] = [model_to_dict(tag) for tag in project.projecttag_set.all()]
@@ -55,3 +60,44 @@ class PetProjectView(APIView):
             return Response(status=404)
 
         return Response(project.id, status=201)
+
+    @swagger_auto_schema(
+        request_body=PetProjectSerializer,
+        request_method="PUT",
+        responses={200: PetProjectSerializer},
+    )
+    def put(self, request):
+        data = request.data
+        project = Project.objects.get(
+            id=request.GET.get("project_id"), user_id=request.user
+        )
+        serializer = PetProjectSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        tags = data.pop("tags")
+
+        Project.objects.filter(
+            id=request.GET.get("project_id"), user_id=request.user
+        ).update(**data)
+        ProjectTag.objects.filter(project_id=project).delete()
+        ProjectTag.objects.bulk_create(
+            [
+                ProjectTag(
+                    project_id=project,
+                    union_type=tag.pop("union_type"),
+                    tag_id=Tags.objects.get_or_create(**tag)[0],
+                )
+                for tag in tags
+            ]
+        )
+        project.save()
+        return Response(project.id, status=201)
+
+    def delete(self, request):
+        try:
+            project = Project.objects.get(
+                id=request.GET.get("project_id"), user_id=request.user
+            ).delete()
+        except Exception:
+            return Response(status=404)
+
+        return Response(status=201)
